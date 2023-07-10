c7aee9f7972e5157713ff90ea31051205d8adaa6
Crowdbotics <engineering@crowdbotics.com>
2023-07-10 12:44:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-feather-dev-99519",
+  url: "https://late-feather-dev-99519.botics.co",
+  api: "https://late-feather-dev-99519.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
